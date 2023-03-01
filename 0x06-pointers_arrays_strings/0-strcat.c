2adeca8430c82381fe9050968730c8705f4e0f2c
Theycallmekennedy <kennedyezuruike@gmail.com>
2023-03-01 15:23:56 -0800

    a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: the string @src is added to
+ * @src: the string to be appended
+ *
+ * Return: pointer to the resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int str1;
+	int str2;
+	int i;
+
+	str1 = 0;
+	str2 = 0;
+
+	while (src[str1] != '\0')
+		str1++;
+
+	while (dest[str2] != '\0')
+		str2++;
+
+	for (i = 0; i <= str1; i++)
+	{
+		dest[str2] = src[i]
+		str2++;
+	}
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates two strings
+ * using at most n bytes from src
+ * @dest - the value of input
+ * @src - the value of input
+ * n - the value of input
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int c, d;
+
+	c = 0;
+
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+
+	d = 0;
+
+	while (str[d] != '\0')
+	{
+		dest[c] = src[d];
+		d++
+		c++;
+	}
+
+	dest[c] = '\0';
+
+	return (dest);
+}
