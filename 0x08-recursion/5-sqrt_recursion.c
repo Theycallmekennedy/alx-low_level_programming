84e9cc8257e71843232630325a1008d4c35bfdb5
Theycallmekennedy <kennedyezuruike@gmail.com>
2023-03-08 05:58:15 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 /**
  *_sqrt_recursion -  func that returns the natural square root of a number.
  * @n: for number input
@@ -16,4 +15,21 @@ int _sqrt_recursion(int n)
 		return (-1);
 	else
 		return (helper(n, 1));
+}
+
+/**
+ * helper - blank
+ * @n: number
+ * @i: incrementor
+ * Return: -1;
+ */
+
+int helper(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n > (i * i))
+		return (helper(n, i + 1));
+	else
+		return (-1);
 }
