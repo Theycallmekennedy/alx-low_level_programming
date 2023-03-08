ddd675ed557fbc7faee7345cabe7c506bda95efe
Theycallmekennedy <kennedyezuruike@gmail.com>
2023-03-08 06:04:21 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,46 @@
 #include "main.h"
+
 /**
- *_sqrt_recursion -  func that returns the natural square root of a number.
- * @n: for number input
- * Return: 0
+ * _sqrt_helper - Returns the natural square root of a number
+ * @n: The number to find the square root of
+ * @i: The current number to check
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root
  */
 
-int _sqrt_recursion(int n)
+int _sqrt_helper(int n, int i)
 {
-	if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	else if (n < 0)
+	if (i * i > n)
+	{
 		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
 	else
-		return (helper(n, 1));
+	{
+		return (_sqrt_helper(n, i + 1));
+	}
 }
 
 /**
- * helper - blank
- * @n: number
- * @i: incrementor
- * Return: -1;
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: The number to find the square root of
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root
+ *
  */
-
-int helper(int n, int i)
+int _sqrt_recursion(int n)
 {
-	if (n == (i * i))
-		return (i);
-	else if (n > (i * i))
-		return (helper(n, i + 1));
-	else
+	if (n < 0)
+	{
 		return (-1);
+	}
+	else
+	{
+		return (_sqrt_helper(n, 0));
+	}
 }
