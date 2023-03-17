d5ac95b070a53cadaa01633b15780d9bfe9dcdde
Theycallmekennedy <kennedyezuruike@gmail.com>
2023-03-16 17:22:23 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings, up to n bytes of the second string
+ * string_nconcat - concatenates two strings
  * @s1: first string to concatenate
  * @s2: second string to concatenate
  * @n: number of bytes of s2 to concatenate
  *
- * Return: pointer to newly allocated memory containing the concatenated string,
- * or NULL if allocation fails
+ * Return: pointer to newly allocated memory containing the concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        char *concat_string;
-        unsigned int len1 = 0, len2 = 0, i;
+	char *concat_string;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-        while (s1[len1])
-                len1++;
+	if (s2 == NULL)
+		s2 = "";
 
-        while (s2[len2])
-                len2++;
+	while (s1[len1])
+		len1++;
 
-        if (n >= len2)
-                n = len2;
+	while (s2[len2])
+		len2++;
 
-        concat_string = malloc(sizeof(char) * (len1 + n + 1));
+	if (n >= len2)
+		n = len2;
 
-        if (concat_string == NULL)
-                return (NULL);
+	concat_string = malloc(sizeof(char) * (len1 + n + 1));
 
-        for (i = 0; i < len1; i++)
-                concat_string[i] = s1[i];
+	if (concat_string == NULL)
+		return (NULL);
 
-        for (i = 0; i < n; i++)
-                concat_string[len1 + i] = s2[i];
+	for (i = 0; i < len1; i++)
+		concat_string[i] = s1[i];
 
-        concat_string[len1 + n] = '\0';
+	for (i = 0; i < n; i++)
+		concat_string[len1 + i] = s2[i];
 
-        return (concat_string);
+	while (n < len2 && i < (len1 + n))
+		concat_string[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		concat_string[i++] = s2[j++];
+
+	concat_string[len1 + n] = '\0';
+
+	return (concat_string);
 }
-
