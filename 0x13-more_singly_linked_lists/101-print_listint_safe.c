6e539891ec1e43bebc1c72ddc87869f614727900
Theycallmekennedy <kennedyezuruike@gmail.com>
2023-04-05 11:47:24 -0700

    a function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,40 +3,42 @@
 #include <stdlib.h>
 
 /**
- * print_listint_safe - This function prints a listint_t linked list
- * @head: Pointer to the first node of the list
- * Return: The number of nodes in the list
+ * print_listint_safe - function that prints a listint_t linked list
+ *
+ * @head: a pointer to the first node of the list
+ *
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current_ptr = head, *check_ptr = head;
+	const listint_t *slow_ptr = head, *fast_ptr = head;
 
-	while (current_ptr && check_ptr && check_ptr->next)
+	while (slow_ptr && fast_ptr && fast_ptr->next)
 	{
-		printf("[%p] %d\n", (void *) current_ptr, current_ptr->n);
+		printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
 		count++;
 
-		current_ptr = current_ptr->next;
-		check_ptr = check_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
 
-		if (current_ptr == check_ptr)
+		if (slow_ptr == fast_ptr)
 		{
-			current_ptr = head;
-			while (current_ptr != check_ptr)
+			slow_ptr = head;
+			while (slow_ptr != fast_ptr)
 			{
-				printf("[%p] %d\n", (void *) current_ptr, current_ptr->n);
+				printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
 				count++;
-				current_ptr = current_ptr->next;
-				check_ptr = check_ptr->next;
+				slow_ptr = slow_ptr->next;
+				fast_ptr = fast_ptr->next;
 			}
-			printf("[%p] %d\n", (void *) current_ptr, current_ptr->n);
+			printf("[%p] %d\n", (void *) slow_ptr, slow_ptr->n);
 			count++;
 			break;
 		}
 	}
 
-	if (!current_ptr || !check_ptr)
+	if (!slow_ptr || !fast_ptr)
 	{
 		while (head)
 		{
